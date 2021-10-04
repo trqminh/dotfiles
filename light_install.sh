48d56a9e910075fd4fe7af3660baf8564689b3bc
trqminh <minhtranquangcr@gmail.com>
2021-10-04 13:56:22 -0500

    fix fzf and viplug for light

--- light_install.sh
+++ light_install.sh
@@ -1,2 +1,20 @@
+FOLD=~/.fzf/
+if [ -d "$FOLD" ]; then
+    echo "[fzf]: OK"
+else
+    echo "INSTALL FZF..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+fi
+
+FILE=~/.vim/autoload/plug.vim
+if [ -f "$FILE" ]; then
+    echo "[vimplug]: OK"
+else
+    echo "INSTALL VIMPLUG..."
+	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
 cp ./lightvimrc ~/.vimrc
 cp ./ideavimrc ~/.ideavimrc
