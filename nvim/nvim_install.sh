c7a10930dc4777460db2be705e1a90d4feb7ec9c
trqminh <minhtranquangcr@gmail.com>
2022-12-21 19:18:18 -0600

    fix nvim errors

--- nvim/nvim_install.sh
+++ nvim/nvim_install.sh
@@ -16,7 +16,8 @@ if [ -f "$FILE" ]; then
     echo "[vimplug for nvim]: OK"
 else 
     echo "INSTALL VIMPLUG for nvim..."
-    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
 echo "[nodejs install]"
