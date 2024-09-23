0ccb3d07be3ee17bd5767aad42c15614595c5a81
minht <minht@bartik.uark.edu>
2024-09-23 17:33:37 -0500

    sigh

--- vim/vim_install.sh
+++ vim/vim_install.sh
@@ -7,21 +7,4 @@ else
     ~/.fzf/install
 fi
 
-FILE=~/.vim/autoload/plug.vim
-if [ -f "$FILE" ]; then
-    echo "[vimplug]: OK"
-else
-    echo "INSTALL VIMPLUG..."
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-VIMDIM=~/.vim/pack/plugins/start/vim-dim
-if [ -d "$VIMDIM" ]; then
-    echo "[vim dim]: OK"
-else
-    echo "INSTALL vim dim color..."
-    git clone --branch 1.x git@github.com:jeffkreeftmeijer/vim-dim.git ~/.vim/pack/plugins/start/vim-dim
-fi
-
-cp vim/vimrc ~/.vimrc
+cp vim/vim_minimal ~/.vimrc
